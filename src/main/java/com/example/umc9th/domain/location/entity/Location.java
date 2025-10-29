58dae0cee76cc3788a44b7c888eea7702d0200f2
IISweetHeartII <sachi009955@gmail.com>
2025-10-29 16:06:57 +0900

    feat: Location, Store 엔티티 추가

---
+++ src/main/java/com/example/umc9th/domain/location/entity/Location.java
@@ -0,0 +1,21 @@
+package com.example.umc9th.domain.location.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
