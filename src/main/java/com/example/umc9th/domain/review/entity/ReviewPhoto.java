a2ed2635b4e5ce105b51aeca50a79fb7b396f8ed
IISweetHeartII <sachi009955@gmail.com>
2025-10-29 16:03:09 +0900

    feat: Review, ReviewPhoto, Reply entitiy 생성성

---
+++ src/main/java/com/example/umc9th/domain/review/entity/ReviewPhoto.java
@@ -0,0 +1,25 @@
+package com.example.umc9th.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "review_photo")
+public class ReviewPhoto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_photo_id")
+    private Long id;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+}
