ab7f86cfeb13c71ec7d2a5106b73ec1005c9e592
IISweetHeartII <sachi009955@gmail.com>
2025-11-19 19:28:02 +0900

    feat: FoodValidadtor 추가

--- src/main/java/com/example/umc9th/global/response/code/ErrorCode.java
+++ src/main/java/com/example/umc9th/global/response/code/ErrorCode.java
@@ -44,7 +44,10 @@ public enum ErrorCode implements BaseCode {
     STORE_NOT_FOUND(404, "STORE_404", "가게를 찾을 수 없습니다"),
 
     // Location
-    LOCATION_NOT_FOUND(404, "LOCATION_404", "지역을 찾을 수 없습니다");
+    LOCATION_NOT_FOUND(404, "LOCATION_404", "지역을 찾을 수 없습니다"),
+
+    // Food
+    FOOD_NOT_FOUND(404, "FOOD_404", "음식 카테고리를 찾을 수 없습니다");
 
     private final int status;
     private final String code;
--- src/main/java/com/example/umc9th/global/validation/validator/FoodExistValidator.java
+++ src/main/java/com/example/umc9th/global/validation/validator/FoodExistValidator.java
@@ -1,17 +1,16 @@
 package com.example.umc9th.global.validation.validator;
 
-import org.springframework.stereotype.Component;
-
 import com.example.umc9th.domain.member.repository.FoodRepository;
+import com.example.umc9th.global.response.code.ErrorCode;
 import com.example.umc9th.global.validation.annotation.ExistFood;
-
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 /**
  * @ExistFood 어노테이션에 대한 검증 로직
- *            Food ID가 DB에 실제로 존재하는지 확인
+ * Food ID가 DB에 실제로 존재하는지 확인
  */
 @Component
 @RequiredArgsConstructor
@@ -31,7 +30,7 @@ public class FoodExistValidator implements ConstraintValidator<ExistFood, Long> 
         if (!isValid) {
             // 기본 메시지를 비활성화하고 커스텀 메시지로 대체
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("해당 음식 카테고리를 찾을 수 없습니다")
+            context.buildConstraintViolationWithTemplate(ErrorCode.FOOD_NOT_FOUND.getMessage())
                     .addConstraintViolation();
         }
 
